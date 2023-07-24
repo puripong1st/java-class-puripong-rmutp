58ef367b379df159398388c8973ac74becc6d6b1
prp <44913650+puripong1st@users.noreply.github.com>
2023-07-24 21:14:40 +0700

    Add files via upload

--- PuripongClassone.java
+++ PuripongClassone.java
@@ -1,14 +1,60 @@
 public class PuripongClassone {
+
     public static void main(String args[]) {
+        String butten = "--------------------------------";
+        // 1
+        System.out.println(butten);
+        System.out.println("Hello World");
+        // 2
+        System.out.println(butten);
+        System.out.print("Hello");
+        System.out.println(" World");
+        // 3
+        System.out.println(butten);
+        System.out.println("   /\\...../\\");
+        System.out.println("  ( .)   (. )");
+        System.out.println(" :::  \\/   :::");
+        System.out.println("  ..       ..");
+        System.out.println("   .  ....  .");
+        System.out.println("   .........");
+        System.out.println("     ,, ,, ");
+
+        // 4
+        System.out.println(butten);
+        System.out.println("_####,____,####");
+        System.out.println("__###,__,####");
+        System.out.println("___####.####'_____,,,,,,,,,,,");
+        System.out.println("____######____,########,,___####____,####");
+        System.out.println("______####_____#########,____####____####");
+        System.out.println("______####____,####____####_,####____####");
+        System.out.println("______####____,###,____####__,####___####");
+        System.out.println("______####_____'####,,####'__'####,,####");
+        System.out.println("______####_______#####__________######");
+        // 5
+        System.out.println(butten);
         String Programname = "* Program name : ";
         String Ver = "* Version : ";
         String Nameprograme = "* Programmer : ";
-        String img ="*************************************";
-        
+        String AboutProgame = "* How to use the Program :";
+        String img = "**********************************************************************";
+
         System.out.println(img);
-        System.out.println(Programname + " Print Hello World *");
-        System.out.println(Ver + "1                       *");
-        System.out.println(Nameprograme + " Puripong Kanekul    *");
+        System.out.println(Programname + " Print Hello World                                  *");
+        System.out.println(Ver + "1                                                        *");
+        System.out.println(Nameprograme + " Puripong Kanekul                                     *");
+        System.out.println(AboutProgame + "This program prints owl pitcture on screen *");
         System.out.println(img);
+        // 6
+        System.out.println(butten);
+        String Name = "Puripong";
+        String Lastname = "Kanekul";
+        String Add = "Donmenug";
+        int Age = 18;
+        String Man = "Man";
+        double Weight = 50.5;
+        float Highth = 175;
+
+        System.out.println(Name + " " + Lastname + " " + Add + " " + Age + " " + Man + " " + Weight + " " + Highth);
+
     }
 }
